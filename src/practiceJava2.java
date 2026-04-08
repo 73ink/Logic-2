05b4d8d5d2b372d9f1e5158d3aa9a79de52e0b13
Aaisha Al Rubaie <gtzx7193@gmail.com>
2026-04-08 10:39:58 +0400

    Task-26: fizzString2 was added

--- src/practiceJava2.java
+++ src/practiceJava2.java
@@ -28,9 +28,11 @@ public class practiceJava2 {
 //        practiceJava2 t24 = new practiceJava2();
 //        System.out.println(t24.less20(18));
         // task-25
-        practiceJava2 t25 = new practiceJava2();
-        System.out.println(t25.answerCell(false, false, false));
+//        practiceJava2 t25 = new practiceJava2();
+//        System.out.println(t25.answerCell(false, false, false));
         // task-26
+        practiceJava2 t26 = new practiceJava2();
+        System.out.println(t26.fizzString2(3));
         // task-27
         // task-28
     }
@@ -181,6 +183,21 @@ public class practiceJava2 {
         }
         else{
             return true;
+        }
+    }
+    public String fizzString2(int n){
+
+        if (n % 3 == 0 && n % 5 == 0){
+            return "FizzBuzz!";
+        }
+        else if (n % 3 == 0){
+            return "Fizz!";
+        }
+        else if (n % 5 == 0){
+            return "Buzz!";
+        }
+        else{
+            return n + "!";
         }
     }
 }
